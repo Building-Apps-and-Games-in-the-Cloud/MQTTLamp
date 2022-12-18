64e74a33c0693cf8e06c2b77882a1cbd793c6708
Rob Miles <rob@robmiles.com>
2022-12-18 11:49:05 +0000

    Fully working

--- MQTTLamp.mjs
+++ MQTTLamp.mjs
@@ -2,16 +2,19 @@ import { InGPIO } from './helpers/InGPIO.mjs';
 import { OutGPIO } from './helpers/OutGPIO.mjs';
 import * as mqtt from "mqtt";
 
+let buttonGPIO = new InGPIO(17, (state) => {
+  let message;
 
-let buttonGPIO = new InGPIO(17, (state)=>
-{
-    console.log(`Sending ${state}`);
-    if(state==0){
-    mqttClient.publish("data","off");
-    }
-    else{
-        mqttClient.publish("data","on");
-    }
+  if (state == 0) {
+    message="off";
+  }
+  else {
+    message = "on";
+  }
+
+  mqttClient.publish("data", message);
+
+  console.log(`Publishing ${message}`);
 });
 
 buttonGPIO.init();
@@ -19,20 +22,20 @@ buttonGPIO.init();
 let lampGPIO = new OutGPIO(4);
 lampGPIO.init();
 
-let mqttClient = mqtt.connect("mqtt://broker.hivemq.com"); 
+let mqttClient = mqtt.connect("mqtt://broker.hivemq.com");
 
-mqttClient.subscribe( "messages");
+mqttClient.subscribe("data");
 
-mqttClient.on("message", (topic,message)=>{
-    console.log(`Received ${message} on ${topic}`);
-    if (message == "on") {
-        lampGPIO.on();
-      }
-    
-      if (message == "off") {
-        lampGPIO.off();
-      }
+mqttClient.on("message", (topic, message) => {
+  console.log(`Received ${message} from ${topic}`);
+  if (message == "on") {
+    lampGPIO.on();
+  }
+
+  if (message == "off") {
+    lampGPIO.off();
+  }
 })
 
-mqttClient.publish("messages","Hello world again");
+mqttClient.publish("messages", "Hello world again");
 
